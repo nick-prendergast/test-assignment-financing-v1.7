b76a9076ddb25100a36d8e10f81dce71309787ae
Ivan Danilov <danilov@crxmarkets.com>
2023-12-08 21:37:25 +0100

    Removed 'test' from package name ad from app main class

--- src/main/java/lu/crx/financing/AssignmentFinancingApplication.java
+++ src/main/java/lu/crx/financing/AssignmentFinancingApplication.java
@@ -8,10 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class TestAssignmentFinancingApplication {
+public class AssignmentFinancingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestAssignmentFinancingApplication.class, args);
+        SpringApplication.run(AssignmentFinancingApplication.class, args);
     }
 
     @Bean
